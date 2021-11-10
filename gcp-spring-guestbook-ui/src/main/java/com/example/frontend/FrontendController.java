4e22309824df7e4adcdd128e09afa211e2bda51c
Marian Venin <marian.venin@gmail.com>
2021-11-10 10:58:33 +0000

    add vision support

--- gcp-spring-guestbook-ui/src/main/java/com/example/frontend/FrontendController.java
+++ gcp-spring-guestbook-ui/src/main/java/com/example/frontend/FrontendController.java
@@ -12,7 +12,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
 import org.springframework.util.StreamUtils;
 import java.io.*;
+import lombok.extern.log4j.Log4j2;
+// Add Vision API imports
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
 
+@Log4j2
 @Controller
 @SessionAttributes("name")
 public class FrontendController {
@@ -21,67 +27,67 @@ public class FrontendController {
     // private PubSubTemplate pubSubTemplate;
     @Autowired
     private OutboundGateway outboundGateway;
-    
-	@Autowired
-	private GuestbookMessagesClient client;
-	
-	@Value("${greeting:Hello}")
-	private String greeting;
 
-        // The ApplicationContext is needed to create a new Resource.
-        @Autowired
-        private ApplicationContext context;
-        // Get the Project ID, as its Cloud Storage bucket name here
-        @Autowired
-        private GcpProjectIdProvider projectIdProvider;
-	
-	@GetMapping("/")
-	public String index(Model model) {
-		if (model.containsAttribute("name")) {
-			String name = (String) model.asMap().get("name");
-			model.addAttribute("greeting", String.format("%s %s", greeting, name));
-		}
-		model.addAttribute("messages", client.getMessages().getContent());
-		return "index";
-	}
-	
-	@PostMapping("/post")
-	// public String post(@RequestParam String name, @RequestParam String message, Model model) {
-        public String post(
-            @RequestParam(name="file", required=false) MultipartFile file,
-              @RequestParam String name,
-              @RequestParam String message, Model model)
-            throws IOException {
-		model.addAttribute("name", name);
+    @Autowired
+    private GuestbookMessagesClient client;
+
+    @Value("${greeting:Hello}")
+    private String greeting;
+
+    // The ApplicationContext is needed to create a new Resource.
+    @Autowired
+    private ApplicationContext context;
+    // Get the Project ID, as its Cloud Storage bucket name here
+    @Autowired
+    private GcpProjectIdProvider projectIdProvider;
+
+    @Autowired
+    private CloudVisionTemplate visionTemplate;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        if (model.containsAttribute("name")) {
+            String name = (String) model.asMap().get("name");
+            model.addAttribute("greeting", String.format("%s %s", greeting, name));
+        }
+        model.addAttribute("messages", client.getMessages().getContent());
+        return "index";
+    }
+
+    @PostMapping("/post")
+    // public String post(@RequestParam String name, @RequestParam String message,
+    // Model model) {
+    public String post(@RequestParam(name = "file", required = false) MultipartFile file, @RequestParam String name,
+            @RequestParam String message, Model model) throws IOException {
+        model.addAttribute("name", name);
         String filename = null;
 
-        if (file != null && !file.isEmpty()
-            && file.getContentType().equals("image/jpeg")) {
-                // Bucket ID is our Project ID
-                String bucket = "gs://" +
-                      projectIdProvider.getProjectId();
-                // Generate a random file name
-                filename = java.util.UUID.randomUUID().toString() + ".jpg";
-                WritableResource resource = (WritableResource)
-                      context.getResource(bucket + "/" + filename);
-                // Write the file to Cloud Storage
-                try (OutputStream os = resource.getOutputStream()) {
-                     os.write(file.getBytes());
-                 }
+        if (file != null && !file.isEmpty() && file.getContentType().equals("image/jpeg")) {
+            // Bucket ID is our Project ID
+            String bucket = "gs://" + projectIdProvider.getProjectId();
+            // Generate a random file name
+            filename = java.util.UUID.randomUUID().toString() + ".jpg";
+            WritableResource resource = (WritableResource) context.getResource(bucket + "/" + filename);
+            // Write the file to Cloud Storage
+            try (OutputStream os = resource.getOutputStream()) {
+                os.write(file.getBytes());
+            }
+             // After writing to GCS, analyze the image.
+             AnnotateImageResponse response = visionTemplate.analyzeImage(resource, Type.LABEL_DETECTION);
+             log.info(response.toString());
         }
 
-		if (message != null && !message.trim().isEmpty()) {
-			// Post the message to the backend service
+        if (message != null && !message.trim().isEmpty()) {
+            // Post the message to the backend service
             // pubSubTemplate.publish("messages", name + ": " + message);
             outboundGateway.publishMessage(name + ": " + message);
-			GuestbookMessage payload = new GuestbookMessage();
-			payload.setName(name);
-			payload.setMessage(message);
-                        // Store the generated file name in the database
-                        payload.setImageUri(filename);
-			client.add(payload);
-		}
-		return "redirect:/";
-  }
+            GuestbookMessage payload = new GuestbookMessage();
+            payload.setName(name);
+            payload.setMessage(message);
+            // Store the generated file name in the database
+            payload.setImageUri(filename);
+            client.add(payload);
+        }
+        return "redirect:/";
+    }
 }
-
